a6344c7617e4445b273cad44fb75e91cbff20df6
misterwhybe <wiebejelsma@hotmail.com>
2023-10-17 22:56:59 +0200

    Trying to set up the basic dapp

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,108 +1,37 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { NextPage } from "next";
+import { Box, Card, CardBody, Container, Flex, Heading, Input, SimpleGrid, Skeleton, Stack, Text } from "@chakra-ui/react";
+import { ConnectWallet, Web3Button, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const contractAddress = "0xF1152B33b3172FF0259AFb894b215D886c188837";
+
+  const {contract} = useContract(contractAddress);
+
+  const {data:totalCoffees, isLoading:loadingTotalCoffees} = useContractRead(contract, "getTotalCoffee");
+  const {data:recentCoffee, isLoading: loadingRecentCoffee} = useContractRead(contract, "getAllCoffee");
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
+    <Container maxW={"1200px"} w={"full"}>
+      <Flex justifyContent={"space-between"} alignItems={"center"} py={"20px"} height={"80px"}>
+        <Box>
+          <Text fontWeight={"bold"}>Buy me a coffee</Text>
+        </Box>
+        <ConnectWallet/>
+      </Flex>
+      <SimpleGrid columns ={2} spacing = {10} mt = {"40px"}>
+        <Box>
+          <Card>
+            <CardBody>
+              <Heading mb={"20px"} >Buy a coffee  </Heading>
+            </CardBody>
+          </Card>
+        </Box>
 
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
+      </SimpleGrid>
+    </Container>
 
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
-      </div>
-    </main>
   );
 };
 
